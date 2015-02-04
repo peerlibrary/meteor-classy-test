218df32097c44d3ad1cbbc962696ad63cc33cf48
Jernej Kos <jernej@kos.mx>
2015-02-04 10:35:52 +0100

    Bumped package version.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:classy-test',
   summary: "Class-based wrapper around tinytest",
-  version: '0.1.0',
+  version: '0.1.1',
   git: 'https://github.com/peerlibrary/meteor-classy-test.git'
 });
 
