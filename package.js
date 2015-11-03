b92210d535953b8714ae2677a9617e15dc908d6a
Mitar <mitar.git@tnode.com>
2015-11-02 19:37:29 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:classy-test',
   summary: "Class-based wrapper around tinytest",
-  version: '0.2.19',
+  version: '0.2.20',
   git: 'https://github.com/peerlibrary/meteor-classy-test.git'
 });
 
