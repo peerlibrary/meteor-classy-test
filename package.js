65efed50262d4e25e048039fa83c8399ecfb1c11
Mitar <mitar.git@tnode.com>
2019-09-19 18:35:50 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'peerlibrary:classy-test',
   summary: "Class-based wrapper around tinytest",
-  version: '0.3.0',
+  version: '0.4.0',
   git: 'https://github.com/peerlibrary/meteor-classy-test.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.0.3_3',
+    'coffeescript@2.4.1',
     'ecmascript',
     'underscore',
     'tinytest',
